ec28b156d957a6555fc3e613ac0ce9db5665b66c
Kenneth Crawford <kcrawford@triumphpay.com>
2022-11-30 23:28:20 -0600

    day 1

---
+++ AoC2022/Day01.fs
@@ -0,0 +1,37 @@
+﻿module Day01
+
+open System
+open AoC2022.Inputs
+
+
+let parseInt str = Int32.Parse(str)
+
+let parseElf (str : String) =
+    str.Split("\n", StringSplitOptions.RemoveEmptyEntries)
+    |> Array.map parseInt
+    |> Array.toSeq
+
+let elves =
+    day01_01.Split("\n\n", StringSplitOptions.RemoveEmptyEntries)
+    |> Array.map parseElf
+    
+let elfSums =
+    elves
+    |> Array.map Seq.sum
+    
+let maxSum =
+    elfSums |> Seq.max
+    
+
+let solveDay0101 = printfn $"%i{maxSum}"
+
+
+let topXSum sums count =
+    sums
+    |> Seq.sortDescending
+    |> Seq.take count
+    |> Seq.sum
+
+let solveDay0102 =
+    let solution = topXSum elfSums 3
+    printfn $"%i{solution}"
---
+++ AoC2022/Program.fs
@@ -0,0 +1,5 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+open Day01
+
+solveDay0101
+solveDay0102
