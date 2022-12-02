c7c2c58a412c0784049e7036d206967130c9add7
Kenneth Crawford <kcrawford@triumphpay.com>
2022-12-01 23:20:07 -0600

    day 2-1

---
+++ AoC2022/Day02.fs
@@ -0,0 +1,62 @@
+﻿module Day02
+
+open System
+open AoC2022.Inputs
+
+type Hand =
+    | Rock
+    | Paper
+    | Scissors
+    
+type Outcome =
+    | Win
+    | Lose
+    | Tie
+    
+let whoWon opponent me =
+    match opponent, me with
+    | (Rock, Paper) -> Win,me
+    | (Rock, Scissors) -> Lose,me
+    | (Paper, Scissors) -> Win,me
+    | (Paper, Rock) -> Lose,me
+    | (Scissors, Rock) -> Win,me
+    | (Scissors, Paper) -> Lose,me
+    | _ -> Tie,me
+    
+let score (outcome, hand) =
+    let handScore = match hand with
+                    | Rock -> 1
+                    | Paper -> 2
+                    | Scissors -> 3
+    let winScore = match outcome with
+                    | Win -> 6
+                    | Lose -> 0
+                    | Tie -> 3
+    handScore + winScore
+    
+
+let parseHand (str : String) =
+    let tokens = str.Split(" ")
+    let opponent = match tokens[0] with
+                   | "A" -> Rock
+                   | "B" -> Paper
+                   | "C" -> Scissors
+                   | _ -> failwithf $"Could not read hand %s{tokens[0]}"
+    let me = match tokens[1] with
+             | "X" -> Rock
+             | "Y" -> Paper
+             | "Z" -> Scissors
+             | _ -> failwithf $"Could not read hand %s{tokens[0]}"
+    (opponent, me)
+
+let hands (str: string) =
+    str.Split("\n", StringSplitOptions.RemoveEmptyEntries)
+    |> Array.map parseHand
+    
+let totalScore (str : string) =
+    hands str
+    |> Array.map (fun (opp,me) -> whoWon opp me)
+    |> Array.map score
+    |> Array.sum
+    
+let solveDay0201 = printfn $"%i{totalScore day0201}"
--- AoC2022/Program.fs
+++ AoC2022/Program.fs
@@ -1,8 +1,11 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
-open Day01
+// open Day01
+// solveDay0101
+// solveDay0102
+//
+// solveDay0101Variant
+// solveDay0102Variant
 
-solveDay0101
-solveDay0102
-
-solveDay0101Variant
-solveDay0102Variant
+open Day02
+solveDay0201
+//solveDay0202
