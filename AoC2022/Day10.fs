58a201b3a427135e257b82160a6d22bc0216edd3
Kenneth Crawford <kcrawford@triumphpay.com>
2022-12-11 01:29:13 -0600

    cleanup

--- AoC2022/Day10.fs
+++ AoC2022/Day10.fs
@@ -4,39 +4,54 @@ open System
 open AoC2022.Inputs.Day10
 open Utils
 
-let cycles = [|20; 60; 100; 140; 180; 220|]
+type CycleState = { cycle : int; value: int }
 
-let execute v command startingCycle =
+module CycleState =
+    let product c = c.cycle * c.value
+
+let importantCycles = [|20; 60; 100; 140; 180; 220|]
+
+let execute reg command startingCycle =
     match splitBy " " command with
-    | [|"noop"|] -> [(startingCycle,v)],v
+    | [|"noop"|] -> [{ cycle = startingCycle; value = reg }],reg
     | [|"addx"; num |] ->
-        [ (startingCycle,v); (startingCycle + 1,v) ], (v + (int num))
+        [
+            { cycle = startingCycle; value = reg };
+            { cycle = startingCycle + 1; value = reg }
+        ], (reg + (int num))
     | _ -> failwith "cant interpret"
 
-let executeAll (v : int) lines=
+let executeAll lines=
     let results,_,_ =
         lines
-        |> Seq.fold (fun (currCycles : (int*int) list, v : int, cycle : int) curr ->
+        |> Seq.fold (fun (currCycles : CycleState list, v : int, cycle : int) curr ->
             let newCycles,newV = execute v curr cycle
-            let allCycles = List.concat [newCycles; currCycles]
+            let allCycles = List.concat [currCycles; newCycles]
             let newCycleNum = cycle + List.length newCycles
             allCycles,newV,newCycleNum
             ) ([],1,1)
+    results
+
+let get1Value cycles =
+    let relevantCycles =
+        cycles
+        |> Seq.filter (fun state -> Seq.contains state.cycle importantCycles)
+    // [for state in x do
+    //      Console.WriteLine(state.cycle.ToString() + "," + state.value.ToString() + " " + (CycleState.product state).ToString())] |> ignore
     
-    let x =
-        results
-        |> Seq.filter (fun (cycleNum : int, _ : int) -> Seq.contains cycleNum cycles)
-    [for a,b in x do
-         Console.WriteLine(a.ToString() + "," + b.ToString() + " " + (a*b).ToString())] |> ignore
-    
-    x
-    |> Seq.map (fun (x,y) -> x * y)
+    relevantCycles
+    |> Seq.map CycleState.product
     |> Seq.fold (+) 0
     
     
 
-let run = splitInputByNewLines >> executeAll 1
+let run = splitInputByNewLines >> executeAll >> get1Value
 
 let print1 = printfn $"{run sample1} {run input1}"
+
+
+
+
+
 
 let print2 = ignore
