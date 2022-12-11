ab9ac3507aed3943c6cab3215335b32722a62ad0
Kenneth Crawford <kcrawford@triumphpay.com>
2022-12-11 11:36:27 -0600

    bigints

--- AoC2022/Day11.fs
+++ AoC2022/Day11.fs
@@ -7,9 +7,9 @@ open Utils
 
 type Monkey = {
     idx : int;
-    items: int list;
-    operation: int -> int;
-    testFn: int -> int
+    items: int64 list;
+    operation: int64 -> int64;
+    testFn: int64 -> int
     timesInspected: int;
 }
 
@@ -18,27 +18,27 @@ module Parsing =
         let stripped = line.Replace("  Operation:", "") |> splitByAll " "
         match stripped with
         | [|"new"; "="; "old"; "*"; "old" |] -> (fun x -> x  * x)
-        | [|"new"; "="; "old"; "*"; x |] -> (*) (int x)
-        | [|"new"; "="; "old"; "+"; x |] -> (+) (int x)
+        | [|"new"; "="; "old"; "*"; x |] -> (*) (int64 x)
+        | [|"new"; "="; "old"; "+"; x |] -> (+) (int64 x)
         | _ -> failwith "unhandled operation"
         
-    let parseTest (line1 : string) (line2 : string) (line3 : string) : (int -> int) =
-        let testFn : (int -> bool) =
+    let parseTest (line1 : string) (line2 : string) (line3 : string) : (int64 -> int) =
+        let testFn : (int64 -> bool) =
             match splitByAll " " (line1.Replace("  Test:", "")) with
-            | [| "divisible"; "by"; divisor |] -> (fun num -> num % (int divisor) = 0 )
+            | [| "divisible"; "by"; divisor |] -> (fun num -> num % (int64 divisor) = 0 )
             | _ -> failwith "cant parse test"
             
         let trueCase = line2.Replace("    If true: throw to monkey ", "") |> int
         let falseCase = line3.Replace("    If false: throw to monkey ", "") |> int
         
-        (fun (num : int) -> if testFn num then trueCase else falseCase)
+        (fun (num : int64) -> if testFn num then trueCase else falseCase)
         
         
 
     let parseMonkey (lines : string[]) =
         {
             idx = int <| lines[0].Replace("Monkey ", "").Replace(":", "");
-            items = lines[1].Replace("Starting items:", "").Replace(" ", "").Split(",") |> Seq.map int |> List.ofSeq;
+            items = lines[1].Replace("Starting items:", "").Replace(" ", "").Split(",") |> Seq.map int64 |> List.ofSeq;
             operation = parseOperation lines[2];
             testFn = parseTest lines[3] lines[4] lines[5]
             timesInspected = 0;
@@ -59,31 +59,31 @@ module Processing =
     let incProcessed =
         Option.map (fun m -> { m with timesInspected = m.timesInspected + 1 })
     
-    let processItem monkey monkeys item =
-        let newWorryLevel = monkey.operation item / 3
+    let processItem worry monkey monkeys (item : int64) =
+        let newWorryLevel = if worry then monkey.operation item / (int64 3) else item
         let newMonkey = monkey.testFn newWorryLevel
         monkeys 
         |> Map.change monkey.idx (removeItem)
         |> Map.change monkey.idx incProcessed
         |> Map.change newMonkey (addItem newWorryLevel)
     
-    let processMonkey monkeys monkey =
+    let processMonkey worry monkeys monkey =
         monkey.items
-        |> Seq.fold (processItem monkey) monkeys
+        |> Seq.fold (processItem worry monkey) monkeys
         
-    let processMonkeys (monkeys : Map<int,Monkey>) =
+    let processMonkeys worry (monkeys : Map<int,Monkey>) =
         seq { 0..(Seq.length monkeys - 1) }
         |> Seq.fold (fun monkeys idx ->
             monkeys
             |> Map.find idx
-            |> processMonkey monkeys
+            |> processMonkey worry monkeys
             ) monkeys
     
     
-let runRounds n str =
+let runRounds worry n str =
     let start = str |> (Parsing.parseAllMonkeys >> Parsing.toMap)
     seq { 1..n }
-    |> Seq.fold (fun acc curr -> Processing.processMonkeys acc) start
+    |> Seq.fold (fun acc curr -> Processing.processMonkeys worry acc) start
 
 let printItems monkeys =
     for idx,monkey in Map.toList monkeys do
@@ -94,7 +94,6 @@ let printItems monkeys =
     Console.WriteLine("")
     Console.WriteLine("")
     monkeys
-    
     
 let score (monkeys : Map<int,Monkey>) =
     monkeys
@@ -107,10 +106,14 @@ let score (monkeys : Map<int,Monkey>) =
 let printScore (score : int) = Console.WriteLine(score)
     
 let print1 =
-    sample1 |> runRounds 1 |> printItems |> (score >> printScore) |> ignore
+    sample1 |> runRounds true 1 |> printItems |> (score >> printScore) |> ignore
     
-    sample1 |> runRounds 20 |> printItems |> (score >> printScore) |> ignore
+    sample1 |> runRounds true 20 |> printItems |> (score >> printScore) |> ignore
     
     
-    input1 |> runRounds 20 |> printItems |> (score >> printScore) |> ignore
+    input1 |> runRounds true 20 |> printItems |> (score >> printScore) |> ignore
     
+    
+let print2 =
+    Console.WriteLine("**************** #2")
+    sample1 |> runRounds false 10000 |> printItems |> (score >> printScore) |> ignore
