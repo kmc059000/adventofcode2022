9dc411abcf2b36995d5ee1ab79daa6b0a9e7495d
Kenneth Crawford <kcrawford@triumphpay.com>
2022-12-12 09:40:45 -0600

    day 10 - 2

--- AoC2022/Day11.fs
+++ AoC2022/Day11.fs
@@ -9,8 +9,9 @@ type Monkey = {
     idx : int;
     items: int64 list;
     operation: int64 -> int64;
-    testFn: int64 -> int
-    timesInspected: int;
+    testFn: int64 -> int;
+    timesInspected: int64;
+    divisor: int;
 }
 
 module Parsing =
@@ -22,26 +23,28 @@ module Parsing =
         | [|"new"; "="; "old"; "+"; x |] -> (+) (int64 x)
         | _ -> failwith "unhandled operation"
         
-    let parseTest (line1 : string) (line2 : string) (line3 : string) : (int64 -> int) =
-        let testFn : (int64 -> bool) =
+    let parseTest (line1 : string) (line2 : string) (line3 : string) : ((int64 -> int) * int) =
+        let (testFn : (int64 -> bool)), divisor =
             match splitByAll " " (line1.Replace("  Test:", "")) with
-            | [| "divisible"; "by"; divisor |] -> (fun num -> num % (int64 divisor) = 0 )
+            | [| "divisible"; "by"; divisor |] -> (fun num -> num % (int64 divisor) = 0 ), (int divisor)
             | _ -> failwith "cant parse test"
             
         let trueCase = line2.Replace("    If true: throw to monkey ", "") |> int
         let falseCase = line3.Replace("    If false: throw to monkey ", "") |> int
         
-        (fun (num : int64) -> if testFn num then trueCase else falseCase)
+        (fun (num : int64) -> if testFn num then trueCase else falseCase), divisor
         
         
 
     let parseMonkey (lines : string[]) =
+        let testFn,divisor = parseTest lines[3] lines[4] lines[5]
         {
             idx = int <| lines[0].Replace("Monkey ", "").Replace(":", "");
             items = lines[1].Replace("Starting items:", "").Replace(" ", "").Split(",") |> Seq.map int64 |> List.ofSeq;
             operation = parseOperation lines[2];
-            testFn = parseTest lines[3] lines[4] lines[5]
-            timesInspected = 0;
+            testFn = testFn;
+            timesInspected = 0
+            divisor = divisor;
         }
         
     let parseAllMonkeys = splitInputByDoubleNewLines >> Array.map (splitInputByNewLines >> parseMonkey)
@@ -57,10 +60,13 @@ module Processing =
         Option.map (fun m -> { m with items = List.concat [m.items; [item]] })
         
     let incProcessed =
-        Option.map (fun m -> { m with timesInspected = m.timesInspected + 1 })
+        Option.map (fun m -> { m with timesInspected = m.timesInspected + (int64 1) })
     
-    let processItem worry monkey monkeys (item : int64) =
-        let newWorryLevel = if worry then monkey.operation item / (int64 3) else monkey.operation item
+    let processItem (worry : int option) monkey monkeys (item : int64) =
+        let newWorryLevel =
+            match worry with
+            | None -> monkey.operation item / (int64 3)
+            | Some m -> monkey.operation item % (int64 m)
         let newMonkey = monkey.testFn newWorryLevel
         monkeys 
         |> Map.change monkey.idx (removeItem)
@@ -82,8 +88,10 @@ module Processing =
     
 let runRounds worry n str =
     let start = str |> (Parsing.parseAllMonkeys >> Parsing.toMap)
+    let divisor = start |> Map.values |> Seq.map (fun x -> x.divisor) |> Seq.fold (*) 1
+    let worryDivisor = if worry then None else Some divisor
     seq { 1..n }
-    |> Seq.fold (fun acc curr -> Processing.processMonkeys worry acc) start
+    |> Seq.fold (fun acc curr -> Processing.processMonkeys worryDivisor acc) start
 
 let printItems monkeys =
     for idx,monkey in Map.toList monkeys do
@@ -109,9 +117,9 @@ let score (monkeys : Map<int,Monkey>) =
     scores
     |> Seq.sortDescending
     |> Seq.take 2
-    |> Seq.fold (*) 1
+    |> Seq.fold (*) (int64 1)
     
-let printScore (score : int) = Console.WriteLine(score)
+let printScore (score : int64) = Console.WriteLine(score)
     
 let print1 =
     sample1 |> runRounds true 1 |> printItems |> (score >> printScore) |> ignore
@@ -124,6 +132,6 @@ let print1 =
     
 let print2 =
     Console.WriteLine("**************** #2")
-    sample1 |> runRounds false 1000 |> printItems |> (score >> printScore) |> ignore
+    sample1 |> runRounds false 10000 |> printItems |> (score >> printScore) |> ignore
     
-    //sample1 |> runRounds false 10000 |> printItems |> (score >> printScore) |> ignore
+    input1 |> runRounds false 10000 |> printItems |> (score >> printScore) |> ignore
