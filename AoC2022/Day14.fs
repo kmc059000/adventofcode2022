8aa8af8d2a3a5222c454bcd0bf5a283968049c3a
Kenneth Crawford <kcrawford@triumphpay.com>
2022-12-16 20:16:34 -0600

    day 14 - 2

--- AoC2022/Day14.fs
+++ AoC2022/Day14.fs
@@ -23,8 +23,12 @@ let parseLine = splitBy " -> " >> Seq.map parseCoords >> Seq.pairwise >> Seq.col
 let parseRocks = splitInputByNewLines >> Seq.collect parseLine >> Seq.map (fun x -> x,Rock) >> Map.ofSeq
 
 let maxY<'a> = Map.keys >> Seq.map snd >> Seq.max
+let maxX<'a> = Map.keys >> Seq.map fst >> Seq.max
+let minX<'a> = Map.keys >> Seq.map fst >> Seq.min
 
 let printState (map : Map<int*int,Cell>) =
+    Console.WriteLine()
+    Console.WriteLine()
     Console.WriteLine()
     for row in (map |> Map.keys |> Seq.map snd |> Seq.min)..(map |> Map.keys |> Seq.map snd |> Seq.max)
         do
@@ -51,8 +55,8 @@ let rec dropSand (state,count) maxY startingPos (x,y) =
         ]
         |> List.filter (fun x -> Option.isNone (Map.tryFind x state))
         
-    if y > maxY then (state,count)
-    else if List.length nextPositions = 0 then
+    if Map.containsKey startingPos state then (state,count)
+    else if y = maxY + 1 || List.length nextPositions = 0 then
         let newState = Map.add (x,y) Sand state
         //printState newState |> ignore
         dropSand (newState,count + 1) maxY startingPos startingPos  
@@ -66,12 +70,29 @@ let inputRocks = parseRocks input1
 let inputMaxY = maxY inputRocks
 
 
-let result = dropSand (inputRocks,0) inputMaxY (500,0) (500,0)
+//let result = dropSand (inputRocks,0) inputMaxY (500,0) (500,0)
 
 let print1 =
-    Console.WriteLine(result)
+    //Console.WriteLine(result)
     ()
+
+
+let addBottom state =
+    let maxY = maxY state
+    let minX = minX state
+    let maxX = maxX state
+    
+    let additional = [for x in (minX - 200)..(maxX + 100) do (x, maxY + 2)]
+    additional
+    |> Seq.fold (fun map curr -> Map.add curr Rock map) state
+
+let p2 input =
+    let rocks = parseRocks input
+    let result = dropSand (rocks,0) (maxY rocks) (500,0) (500,0)
+    printState (fst result)
+    result
+
 
 
 let print2 =
-    ()
+    (Console.WriteLine(p2 input1 |> snd))
