ca498b304278ce564929e493866b860ace6c091f
Kenneth Crawford <kcrawford@triumphpay.com>
2023-12-03 21:15:35 -0800

    day 4 1

--- AoC2023/Day04.fs
+++ AoC2023/Day04.fs
@@ -1,20 +1,28 @@
 ﻿module Day04
 
 open System
-open System.Text.RegularExpressions
 open Utils
-open AoC2023.Inputs.Day03
+open AoC2023.Inputs.Day04
 
-let solve1 _ = ""
+let parseLine line =
+    let mainTokens = (splitBy ":" line)
+    let tokens = mainTokens[1] |> splitBy "|"
+    let winningNumbers = tokens.[0] |> splitBy " " |> Array.map int |> Set.ofArray
+    let ourNumbers = tokens.[1] |> splitBy " " |> Array.map int |> Set.ofArray
+    let winningCount = ourNumbers |> Seq.filter (fun x -> Set.contains x winningNumbers) |> Seq.length
+    let pow = (winningCount - 1)
+    Math.Pow(2, pow) |> int
+
+let solve1 = splitInputByNewLines >> Seq.map parseLine >> Seq.sum
 
 let solve2 _ = ""
 
 let print1 =
-    Console.WriteLine(solve1 day3p1example)
-    Console.WriteLine(solve1 day3p1)
+    Console.WriteLine(solve1 example1)
+    Console.WriteLine(solve1 p1)
     ()
    
 let print2 =
-    Console.WriteLine(solve2 day3p1example)
-    Console.WriteLine(solve2 day3p1)
+    Console.WriteLine(solve2 example1)
+    Console.WriteLine(solve2 p1)
     ()
