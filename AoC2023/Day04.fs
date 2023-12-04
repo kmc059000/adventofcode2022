bc07ef93a201f85e541bee749374ba033e6e7e6d
Kenneth Crawford <kcrawford@triumphpay.com>
2023-12-03 20:59:43 -0800

    setup day 4

---
+++ AoC2023/Day04.fs
@@ -0,0 +1,20 @@
+﻿module Day04
+
+open System
+open System.Text.RegularExpressions
+open Utils
+open AoC2023.Inputs.Day03
+
+let solve1 _ = ""
+
+let solve2 _ = ""
+
+let print1 =
+    Console.WriteLine(solve1 day3p1example)
+    Console.WriteLine(solve1 day3p1)
+    ()
+   
+let print2 =
+    Console.WriteLine(solve2 day3p1example)
+    Console.WriteLine(solve2 day3p1)
+    ()
