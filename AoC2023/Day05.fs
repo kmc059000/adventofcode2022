a818b5d758ad42256f82a6858c670a80da8b55cb
Kenneth Crawford <kcrawford@triumphpay.com>
2023-12-04 21:50:08 -0800

    working day 5 1

--- AoC2023/Day05.fs
+++ AoC2023/Day05.fs
@@ -4,35 +4,48 @@ open System
 open Utils
 open AoC2023.Inputs.Day05
 
+type NumMap = int64 * int64 * int64
+
+let doMap value numMap =
+    let (sourceStart, destStart, length) = numMap
+    let sourceEnd = sourceStart + length
+    let destEnd = destStart + length
+    if value >= sourceStart && value < sourceEnd then
+        let offset = value - sourceStart
+        Some (destStart + offset)
+    else
+        None
+
+let transition maps value =
+    Seq.choose (doMap value) maps
+    |> Seq.tryHead
+    |> Option.defaultValue value
+    
+    
+
 let parseLine (input : string) =
     let tokens = input |>  splitBy " " |> Seq.map int64 |> Seq.toList
-    let sourceStart, destStart, length =
-        match tokens with
-        | [destStart; sourceStart; length] -> (sourceStart, destStart, length)
-        | _ -> failwith "Invalid input"
-    
-    Seq.zip [sourceStart..sourceStart + length] [destStart..destStart + length]
-    |> Map.ofSeq
+    match tokens with
+    | [destStart; sourceStart; length] -> NumMap (sourceStart, destStart, length)
+    | _ -> failwith "Invalid input"
 
-let parseMap: string -> Map<int64,int64> = splitInputByNewLines >> Seq.skip 1 >> Seq.map parseLine >> mergeManyMaps
+let parseMap = splitInputByNewLines >> Seq.skip 1 >> Seq.map parseLine >> Seq.toList
 
 let parseMaps (input:string[]) =
     let seeds = input[0l] |> splitBy " " |> Seq.skip 1 |> Seq.map int64 |> List.ofSeq
     let maps = input.[1l..] |> Seq.map parseMap |> List.ofSeq
     (seeds, maps)
     
-let minLocation maps seed =
+let minLocation (maps : NumMap list list) seed =
     maps
     |> Seq.fold (fun acc currMap ->
-           let next = Map.tryFind acc currMap
-                      |> Option.defaultValue acc
-           
+           let next = transition currMap acc           
            printfn $"%i{acc} -> %i{next}"
            next
     ) seed
     
     
-let minLocationOfAllSeeds (seeds : int64 list, maps : Map<int64, int64> list) =
+let minLocationOfAllSeeds (seeds : int64 list, maps : NumMap list list) =
     Seq.map (minLocation maps) seeds |> Seq.min
     
 let solve1 = splitInputByDoubleNewLines >> parseMaps >> minLocationOfAllSeeds
