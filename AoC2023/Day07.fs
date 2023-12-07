9c1a6c487f91ba6fa1235d4f0758e4422037d263
Kenneth Crawford <kcrawford@triumphpay.com>
2023-12-06 21:48:36 -0800

    day 7 1

--- AoC2023/Day07.fs
+++ AoC2023/Day07.fs
@@ -2,7 +2,56 @@
 open Utils
 open AoC2023.Inputs.Day07
 
-let solve1 = id
-let solve2 = id
+type Hand = {
+    handType : int
+    values : int array
+    bid: int
+    rawHand : string
+}
+
+let valueMap = "23456789TJQKA".ToCharArray() |> Array.mapi (fun i c -> c, i) |> Map.ofArray
+
+let parseHand (cards: string, bid : string) =
+    let values =
+        cards.ToCharArray()
+        |> Array.map (flip Map.find valueMap)
+    
+    let rankGroups = values |> Seq.groupBy id |> Seq.map (Tuple2.mapSnd Seq.length) |> Seq.map snd |> Seq.sortByDescending id |> List.ofSeq
+    
+    let handType =
+        match rankGroups with
+        | [ 5 ] -> 7
+        | [ 4; _ ] -> 6
+        | [ 3; 2 ] -> 5
+        | 3::_ -> 4
+        | 2::2::_ -> 3
+        | 2::_ -> 2
+        | _ -> 1
+        
+    { handType = handType; values = values; bid = int bid; rawHand = cards }
+     
+let parseLine line =
+    line
+    |> splitBy2 " "
+    |> parseHand
+
+let parse = splitInputByNewLines >> Seq.map parseLine >> Seq.toList
+
+let compareHands (h1 : Hand) (h2 : Hand) =
+    match compare h1.handType h2.handType with
+    | 0 -> compare h1.values h2.values
+    | x -> x
+
+let solve hands =
+    hands
+    |> Seq.sortWith compareHands
+    |> Seq.mapi (fun i h -> i + 1, h)
+    |> tapValues
+    |> Seq.map (fun (rank,h) -> h.bid * rank)
+    |> Seq.sum
+    
+
+let solve1 = parse >> solve
+let solve2 _ = -1
 
 let printAnswer = printAnswersWithSameInputs solve1 solve2 example1 p1
--- AoC2023/Utils.fs
+++ AoC2023/Utils.fs
@@ -126,3 +126,31 @@ let printAnswers solvePart1 e1 p1 solvePart2 e2 p2 =
     printfn ""
     
 let printAnswersWithSameInputs solve1 solve2 e1 p1 = printAnswers solve1 e1 p1 solve2 e1 p1
+
+
+module Tuple2 =
+    let replicate x = x, x
+
+    let curry f x y = f (x, y)
+
+    let uncurry f (x, y) = f x y
+
+    let swap (x, y) = (y, x)
+
+    let mapFst f (x, y) = f x, y
+
+    let mapSnd f (x, y) = x, f y
+
+    let extendFst f (x,y) = f (x,y), y
+
+    let extendSnd f (x,y) = x, f(x,y)
+
+    let optionOfFst f (x, y) =
+        match f x with
+        | Some x' -> Some (x', y)
+        | None    -> None
+
+    let optionOfSnd f (x, y) =
+        match f y with
+        | Some y' -> Some (x, y')
+        | None    -> None
